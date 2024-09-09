cad8d2f70315addeccf771a3618ed59852a9a1d4
Joshua <amethystguitaristj@gmail.com>
2024-09-09 16:57:39 +0100

    attempt to add websocket to the application

--- src/app.js
+++ src/app.js
@@ -3,12 +3,33 @@ const os = require('os')
 const express = require('express')
 const expressApp = require('./hls_server/server')
 const path = require('path')
+const websocket = require("websocket")
+const ws = require("ws")
 
 const cpus = os.cpus();
 
 if (cpus.length > 0) {
     
 }
+
+//add the websocket connection to send ffmpeg conversion process
+const webSocketServer = new ws.Server({
+    noServer:true
+})
+
+webSocketServer.on("connection",(websock)=>{
+    websock.on("message",(data,isBinary)=>{
+    })
+
+    websock.on("close",(code,reason)=>{
+        console.log(reason)
+    })
+    
+    websock.on("error",(err)=>{
+        console.log(err)
+    })
+
+})
 
 expressApp.use(express.static(path.join(__dirname,'template')))
 expressApp.use(express.static(path.join(__dirname,'utils')))
