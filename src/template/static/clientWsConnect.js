6b9ceb0029433667441f38273b93aca91549aaa8
Joshua <amethystguitaristj@gmail.com>
2024-09-11 11:20:43 +0100

    ending touches to the Websockets client side

--- src/template/static/clientWsConnect.js
+++ src/template/static/clientWsConnect.js
@@ -19,8 +19,57 @@ window.addEventListener("DOMContentLoaded",(ev)=>{
     })
     websocketConnection.addEventListener("message",(ev)=>{
         const data = JSON.parse(ev.data)
-        //create a container to show this data
-        console.log(ev.data)
+        let getHLSProcesses = sessionStorage.getItem("getHLSProcesses") || "[]"
+        let HLSProcesses = JSON.parse(getHLSProcesses)
+        let isExistent = false
+        for (let index = 0; index < HLSProcesses.length; index++) {
+            if (HLSProcesses[index].fileId == data.fileId) {
+                HLSProcesses[index] = data
+                isExistent = true
+                if(data.completed){
+                    HLSProcesses.splice(HLSProcesses.indexOf(HLSProcesses[index]))
+                }
+            }
+        }
+        if(!isExistent){
+            HLSProcesses.push(data)
+        }
+        //save data in sessionStorage
+        sessionStorage.setItem("getHLSProcesses",JSON.stringify(HLSProcesses))
+
+        if (HLSProcesses.length != 0) {
+            const body = document.body
+            let exisingProgressCont = document.querySelector(".hls-progress-cont")
+            if (exisingProgressCont) {
+                body.removeChild(exisingProgressCont)
+            }
+            let progressCont = exisingProgressCont || document.createElement("div")
+            //remove all the children of progress container
+            while (progressCont.firstChild) {
+                progressCont.removeChild(progressCont.lastChild);
+            }
+            progressCont.classList.add("hls-progress-cont","glassy")
+
+            let allProgressBoxes = ""
+            //create a container to show this data
+            HLSProcesses.forEach((hlsprocess)=>{
+                let progressBox = `
+                <div class="progress-box">
+                    <h2 class="progress-title">${hlsprocess.name}</h2>
+                    <p class="progress-slug">${hlsprocess.fileId}</p>
+                    <span class="barr">
+                        <span class="progress-bar-container">
+                            <span class="progress-bar" style="width:${Math.round(hlsprocess.progress)}%"></span>
+                        </span>
+                        <p class="percent">${Math.round(hlsprocess.progress)}%</p>
+                    </span>
+                </div>
+                `
+                allProgressBoxes += progressBox
+            })
+            progressCont.innerHTML = allProgressBoxes
+            body.appendChild(progressCont)
+        }
     })
     websocketConnection.addEventListener("error",(ev)=>{
         console.log(ev)
