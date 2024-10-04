346beb5b2da2681d64dc2da4aa42aba43c9592c6
Joshua <amethystguitaristj@gmail.com>
2024-10-04 21:27:27 +0100

    added cdn files to be hosted on the server

--- src/sources/ftp.js
+++ src/sources/ftp.js
@@ -2,7 +2,19 @@ const ftp = require('ftp-client')
 
 
 const connectToFTP = ()=>{
-    const Client = new ftp({host: ip_address,port: 21,user: username, password: password},{logging: 'basic'})//assumes port is 21
+    const Client = new ftp(
+        {host: ip_address,port: 21,user: username, password: password},
+        {logging: 'debug'}
+    )//assumes port is 21
     Client.ftp.connect()
-    
+    return Client.ftp
+}
+
+const downloadFile = () => {
+    //connect to ftp server
+    let FTPServer = connectToFTP()
+    FTPServer.list("/",(err,listing)=>{
+        if (err) console.log(err)
+        console.log(listing)
+    })
 }
